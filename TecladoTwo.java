5c2e06f3b4772a45e170a3b43f1acbaf25bf1815
Leonardo Nascimento <leonardo@14bisdigital.com.br>
2016-03-28 17:23:48 -0300

    What your name?

---
+++ TecladoTwo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TecladoTwo {
+		public static void main (String[] args) {
+		System.out.println("Qual o seu nome? ");
+		Scanner in = new Scanner (System.in);
+		System.out.println("Uaal, sempre achei bacana o nome " + in.nextLine());
+	}
+}
