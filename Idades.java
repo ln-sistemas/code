b232c6862873813dac95677831d40a0b755b029f
Leonardo Nascimento <leonardo@14bisdigital.com.br>
2016-03-29 15:43:41 -0300

    Alterado nome da função que estava causando bugs

---
+++ Idades.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Idades {
+		public static void main (String[] args) {
+		System.out.println("Qual o seu nome ??");
+		Scanner in = new  Scanner (System.in);
+		System.out.println(in.nextLine() + ", que nome engraçado haha");
+	}
+}
